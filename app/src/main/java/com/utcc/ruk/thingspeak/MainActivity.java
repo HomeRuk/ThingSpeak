ac2f73dcd82e87e8eba6e171ad31f89debe7b38b
pr3a <5605104046@live4.utcc.ac.th>
2016-06-09 10:34:40 +0700

    Edit mainActivity.java

--- app/src/main/java/com/utcc/ruk/thingspeak/MainActivity.java
+++ app/src/main/java/com/utcc/ruk/thingspeak/MainActivity.java
@@ -6,11 +6,9 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
-
 //import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
