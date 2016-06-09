3a1d84dd65482faa7028c72ad885ab6e5534620c
pr3a <5605104046@live4.utcc.ac.th>
2016-06-09 10:34:40 +0700

    Edit mainActivity.java

--- app/src/main/java/com/utcc/ruk/thingspeak/MainActivity.java
+++ app/src/main/java/com/utcc/ruk/thingspeak/MainActivity.java
@@ -2,7 +2,6 @@ package com.utcc.ruk.thingspeak;
 
 //import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
